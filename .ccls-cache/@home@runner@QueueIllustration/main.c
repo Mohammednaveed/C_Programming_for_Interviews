80a616813e3eb1a3a4fc4f8ee355f8324b4b2666
Shaik Mohammed Naveed <83392132+Mohammednaveed@users.noreply.github.com>
2023-04-17 04:15:04 +0000

    Added Linear Queue Code

--- .ccls-cache/@home@runner@QueueIllustration/main.c
+++ .ccls-cache/@home@runner@QueueIllustration/main.c
@@ -1,3 +1,8 @@
+// PROGRAM FOR CIRCULAR QUEUE
+// Circular Queue is just a variation of the linear queue in which front and
+// rear-end are connected to each //other to optimize the space wastage of the
+// Linear queue and make it efficient
+
 #include <stdio.h>
 #define max 5
 int f = -1, r = -1;
@@ -22,6 +27,8 @@ int main() {
     case 3:
       display();
       break;
+    case 4:
+      break;
     default:
       printf("Invalid Input\n");
     }
@@ -29,36 +36,39 @@ int main() {
 }
 void enqueue() {
   int ele;
-  if (r == max - 1) {
+  if ((r + 1) % max == f) {
     printf("Queue Full!!\n");
     return;
   }
   printf("Enter the Element : ");
   scanf("%d", &ele);
-  queue[++r] = ele;
+  r = (r + 1) % max;
+  queue[r] = ele;
   printf("%d Queued Successfully \n", ele);
   if (f == -1)
     f = 0;
 }
 void dequeue() {
   int ele;
-  if (f == r || f == -1) {
+  if (f == -1) {
     printf("Queue is Empty!!\n");
     return;
   }
-  ele = queue[f++];
+  ele = queue[f];
   printf("%d is Dequeued Successfully \n", ele);
   if (f == r) {
-    f = r = -1;
-  }
+    f = r = -1; // Reinitialise to -1 when there is no element present
+  } else
+    f = (f + 1) % max;
 }
 void display() {
   int i;
-  if (f == r || f == -1) {
+  if (f == -1) {
     printf("Queue is Empty\n");
     return;
   }
   printf("Queue Elements are \n");
-  for (i = f; i <= r; i++)
+  for (i = f; i != r; i = (i + 1) % max)
     printf("%d\t", queue[i]);
+  printf("%d\t", queue[i]);
 }
